d0e2b803dbc7ec37f0d6a63b5d56548706d284e4
mujahid <mo.mujah@gmail.com>
2014-03-21 23:38:23 +0800

    spring security bug fix

--- app/src/main/java/com/quizcomm/controller/LoginController.java
+++ app/src/main/java/com/quizcomm/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.quizcomm.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,10 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/loginfailed")
-	public String loginFailed(ModelMap model){
-		model.addAttribute("failed", "failed");
-		return "loginfailed";
+	public String loginFailed(HttpSession session,ModelMap model){
+		model.addAttribute("error", "true");
+		session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION");
+		return "login";
 	}
 
 }
